6c907fbea82930938d60f889ff1c3eb7fa859c10
Rohit Goyal <rohit157.rg@gmail.com>
2026-03-25 15:40:32 +0530

    Added Login Integration and Auth Middleware

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router";
+import { Provider } from "react-redux";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import store from "./utils/store";
+import Feed from "./components/Feed";
+import AuthLayout from "./components/layouts/AuthLayout";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              {/*** Used As an middleware */}
+              <Route element={<AuthLayout />}>
+                <Route path="/" element={<Feed />} />
+                <Route path="/profile" element={<Profile />} />
+              </Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,71 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/slices/userSlice";
+import { useNavigate } from "react-router";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const signIn = async () => {
+    try {
+      const result = await fetch("http://localhost:5000/login", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          emailId,
+          password,
+        }),
+      });
+
+      const json = await result.json();
+      dispatch(addUser(json.data));
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleSignIn = () => {
+    signIn();
+  };
+  return (
+    <div className="flex justify-center">
+      <div className="card bg-base-300 w-96 shadow-sm my-[5%]">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Sign in</h2>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email Address</legend>
+            <input
+              type="email"
+              className="input"
+              placeholder="Email Address"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="password"
+              className="input"
+              placeholder="Password"
+              password={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </fieldset>
+          <div className="card-actions justify-center my-2">
+            <button className="btn btn-primary" onClick={handleSignIn}>
+              Sign In
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
